8757efdc5aade78fbc2ba2f93258222ee10df6d6
Ilya91 <webmaster.1991@mail.ru>
2016-10-18 18:39:21 +0300

    database connection

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+try{
+    $db = new PDO('mysql:host=localhost;dbname=session', 'sess_admin', 'secret');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    $error = $e->getMessage();
+}
+if(isset($db)){
+    echo 'Connected';
+}elseif (isset($error)){
+    echo $error;
+}else{
+    echo 'Unknown error';
+}
